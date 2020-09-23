11eb303b076c9dacd831a30a50fc469314fd137b
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-09-22 22:28:24 -0500

    created a function and added the starting variables

--- script.js
+++ script.js
@@ -10,5 +10,27 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  /* alphabet and special chars are a string that will be accessed later */
+  var alphaBet = 'abcdefghijklmnopqrstuvwxyz';
+  var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  /* prompt the user to enter a password length */
+  var pLength = prompt("How long do you want the password to be? (8 to 128)");
+
+  /* comfirm messages to ask the user what criteria they want */
+  var yesLower = confirm("Do you want lower case letters?");
+  var yesUpper = confirm("Do you want upper case letters?");
+  var yesNums = confirm("Do you want numbers?");
+  var yesSpecial = confirm("Do you want special characters?");
+
+  /* if user chooses no criteria they will be asked to choose again */
+  if (yesUpper==false && yesLower==false && yesNums==false && yesSpecial==false) {
+    alert("You must choose at least one criteria");
+    generatePassword()
+  }
+  return("123")
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
