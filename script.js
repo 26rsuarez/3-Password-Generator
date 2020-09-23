0fa7922c000d36bf564d30a3baf7194f21954e49
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-09-22 22:45:32 -0500

    added a while loop to prompt user to chose a right length

--- script.js
+++ script.js
@@ -18,13 +18,19 @@ function generatePassword() {
   /* prompt the user to enter a password length */
   var pLength = prompt("How long do you want the password to be? (8 to 128)");
 
+  /* if length given by user is too short or too long alert and restart process */
+  while (pLength<8 || pLength>128) {
+    alert("please choose a length between 8 and 128 characters!");
+    var pLength = prompt("How long do you want the password to be? (8 to 128)");
+  }
+
   /* comfirm messages to ask the user what criteria they want */
   var yesLower = confirm("Do you want lower case letters?");
   var yesUpper = confirm("Do you want upper case letters?");
   var yesNums = confirm("Do you want numbers?");
   var yesSpecial = confirm("Do you want special characters?");
 
-  /* if user chooses no criteria they will be asked to choose again */
+  /* if user chooses no criteria they will be asked to start again */
   if (yesUpper==false && yesLower==false && yesNums==false && yesSpecial==false) {
     alert("You must choose at least one criteria");
     generatePassword()
